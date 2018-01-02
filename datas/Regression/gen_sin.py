7c2d7459522d30b89ba24b105a50ba442ceb1b37
louie.zl <louie.zl@alibaba-inc.com>
2018-01-02 21:31:01 +0800

    add

--- datas/Regression/gen_sin.py
+++ datas/Regression/gen_sin.py
@@ -34,21 +34,24 @@ class line_data(base_data):
 class GMM_data(base_data):
     def __init__(self, data_len, dim=1, mu=0, sigma=1, mixture=1, aplha=1):
         base_data.__init__(self, data_len, dim)
-        self.y = np.zeros([data_len, data_len], dtype= float)
         if dim > 1:
-            self.x = np.matrix(np.random.rand(data_len, dim))
+            self.y = np.zeros(data_len, dtype=float)
+            self.x = np.matrix(np.sort(np.random.rand(data_len, dim)))
             for i in range(mixture):
-                print mu[i],sigma[i]
+                # print mu[i],sigma[i]
                 # print self.x.shape,mu[i].shape,np.matrix(self.x - mu[i])
                 # print np.linalg.inv(sigma[i])
                 # print np.matrix(self.x - mu[i]) * np.linalg.inv(sigma[i])
-                print np.linalg.det(sigma[i])
+                # print np.linalg.det(sigma[i])
                 self.y += np.diag(aplha[i] * 1 / np.sqrt(2 * np.pi * np.abs(np.linalg.det(sigma[i]))) * np.exp(-np.matrix(self.x - mu[i]) * np.linalg.inv(sigma[i]) * np.matrix(self.x - mu[i]).T))
         else:
-            self.x = np.random.rand(data_len, dim)
+            self.x = np.sort(np.random.rand(data_len))
+            print self.x
+            print '-------'
+            self.y = np.zeros(data_len, dtype=float)
             for i in range(mixture):
-                self.y += aplha[i] * 1 / np.sqrt(2 * np.pi * sigma[i]) * np.exp(
-                    -(self.x - mu[i]) * (1 / sigma[i]) * (self.x - mu[i]))
+                print self.y.shape
+                self.y += aplha[i] * 1 / np.sqrt(2 * np.pi * sigma[i]) * np.exp(-(self.x - mu[i]) * (1 / sigma[i]) * (self.x - mu[i]))
 
 
 class norm_data(GMM_data):
@@ -66,8 +69,8 @@ class RGS_datas(object):
         self.data_len = data_len
         self.dim = dim
         self.linear = line_data(100,1,2,4)
-        self.GMM2 = GMM_data(100,2,np.array([[0,1]]),np.array([[[2,3],[4,5]]]),1,[1])
-        self.GMM = GMM_data(100,2,np.array([[0,1]]),np.array([[[2,3],[4,5]]]),1,[1])
+        self.GMM2 = GMM_data(100,2,np.array([[0.5,0.5]]),np.array([[[2,3],[4,5]]]),1,[1])
+        self.GMM = GMM_data(100,1,[0.5],[1],1,[1])
         self.sin = sin_data(100,1,1)
 
     def set_linear(self,w,b):
@@ -95,13 +98,18 @@ class RGS_datas(object):
     #         self.y =
 
     def show_data(self,args="b-"):
+        # plt.plot(self.GMM.x, self.GMM.y, args)
+        # print self.GMM.y.shape
+        # plt.show()
         if self.dim == 1 :
             plt.plot(self.sin.x,self.sin.y,args)
-            plt.show()
+            # plt.show()
         if self.dim == 2:
             fig = plt.figure()
             ax = Axes3D(fig)
-            print self.GMM2.y
+            print '------'
+            print self.GMM2.y.shape
+
             ax.plot_surface(self.GMM2.x[:,0], self.GMM2.x[:,1], self.GMM2.y, rstride=1, cstride=1, cmap='rainbow')
             plt.show()
 
